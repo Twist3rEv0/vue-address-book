363498dc9091d10bf231ef6aa86382c9fe2b622a
Giulio Giarratana <giarratana.giulio@gmail.com>
2023-11-04 17:18:58 +0100

    init() - initial scaffolding project

---
+++ src/components/contacts-list/ContactsList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="contacts-list">todo list</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ContactsList",
+});
+</script>
+
+<style scoped lang="scss">
+@import "ContactsList";
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <ContactsList />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ContactsList from "@/components/contacts-list/ContactsList.vue";
+
+export default defineComponent({
+  name: "HomeView",
+  components: { ContactsList },
+});
+</script>
